a5ac10512ae7a6192d322e2bfe63e8bf4b3c0827
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-03-17 21:30:05 -0300

    Codigo do cartao

---
+++ stateless_widget_app/stateless_widget_app/lib/constantes.dart
@@ -0,0 +1,7 @@
+final CaminhoImagens = <String,String>{
+  "IMAGEM_PESSOAL":"assets/images/imagem_pessoal.jpg",
+  "LOGO_GITHUB":"images/logo_github.png",
+  "LOGO_LINKEDIN":"images/logo_linkedin.png",
+  "QRCODE_GITHUB":"images/qr_code_github.png",
+  "QRCODE_LINKEDIN":"images/qr_code_linkedin.png",
+};
--- stateless_widget_app/stateless_widget_app/lib/main.dart
+++ stateless_widget_app/stateless_widget_app/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:stateless_widget_app/constantes.dart';
 
 void main() => runApp(MeuAppCartao());
 
@@ -21,6 +23,28 @@ class MinhaTelaCartao extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Meu Cartão Pessoal"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(CaminhoImagens["IMAGEM_PESSOAL"]),
+          Text("Murilo Zanini de Carvalho", style: TextStyle(fontSize: 24.0),),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(CaminhoImagens["LOGO_GITHUB"]),
+              Image.asset(CaminhoImagens["QRCODE_GITHUB"]),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(CaminhoImagens["LOGO_LINKEDIN"]),
+              Image.asset(CaminhoImagens["QRCODE_LINKEDIN"]),
+            ],
+          ),
+        ],
       ),
     );
   }
