17b29343e950ad6c3951e13ea55897688f48dd59
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-03-17 21:46:12 -0300

    Ajustes nas imagens

--- stateless_widget_app/stateless_widget_app/lib/main.dart
+++ stateless_widget_app/stateless_widget_app/lib/main.dart
@@ -24,27 +24,62 @@ class MinhaTelaCartao extends StatelessWidget {
       appBar: AppBar(
         title: Text("Meu Cartão Pessoal"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset(CaminhoImagens["IMAGEM_PESSOAL"]),
-          Text("Murilo Zanini de Carvalho", style: TextStyle(fontSize: 24.0),),
-          Row(
+      body: Container(
+        color: Colors.orange,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Image.asset(CaminhoImagens["LOGO_GITHUB"]),
-              Image.asset(CaminhoImagens["QRCODE_GITHUB"]),
+              // Image.asset(CaminhoImagens["IMAGEM_PESSOAL"]),
+              CircleAvatar(
+                backgroundImage: AssetImage(CaminhoImagens["IMAGEM_PESSOAL"]),
+                radius: 100.0,
+
+              ),
+              Text("Murilo Zanini de Carvalho", style: TextStyle(fontSize: 24.0),),
+
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Image.asset(CaminhoImagens["LOGO_GITHUB"]),
+                    CircleAvatar(
+                      backgroundImage: AssetImage(CaminhoImagens["LOGO_GITHUB"]),
+                      radius: 80.0,
+                      backgroundColor: Colors.white,
+                    ),
+                    SizedBox(width: 24.0,),
+                    Image.asset(CaminhoImagens["QRCODE_GITHUB"]),
+
+
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Image.asset(CaminhoImagens["LOGO_LINKEDIN"]),
+                    CircleAvatar(
+                      backgroundImage: AssetImage(CaminhoImagens["LOGO_LINKEDIN"]),
+                      radius: 80.0,
+                      backgroundColor: Colors.white,
+                    ),
+                    SizedBox(width: 24.0,),
+                    //Image.asset(CaminhoImagens["QRCODE_LINKEDIN"]),
+                    CircleAvatar(
+                      backgroundImage: AssetImage(CaminhoImagens["QRCODE_LINKEDIN"]),
+                      radius: 120.0,
+                      backgroundColor: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(CaminhoImagens["LOGO_LINKEDIN"]),
-              Image.asset(CaminhoImagens["QRCODE_LINKEDIN"]),
-            ],
-          ),
-        ],
+        ),
       ),
     );
   }
